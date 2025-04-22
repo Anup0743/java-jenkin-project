271f3a517a758ddc0ee1a08493d3791dd1e42dab
Anup Jain <anuptyaral28@gmail.com>
2025-04-22 16:19:21 +0530

    this seond  commit

--- Main.java
+++ Main.java
@@ -6,6 +6,8 @@ public class Main
         System.out.println("this is java program");
         System.out.println("Output from java file");
         System.out.println(new Date());
+        System.out.println("This is new ate "+new Date());
+
         
     }
 }
